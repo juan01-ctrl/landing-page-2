996f7a59a18cadaa1dccc45c805de3dc728858d0
juan01-ctrl <juanierace2@gmail.com>
2021-01-12 22:57:54 -0300

    Updated

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -34,3 +34,40 @@ document.addEventListener('click',e=>{
         menu.classList.remove('active')
    
 })
+
+
+//modal form
+
+
+  const $form = document.querySelector(".contact-form"),
+    $loader = document.querySelector(".contact-form-loader"),
+    $response = document.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/your@email.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
