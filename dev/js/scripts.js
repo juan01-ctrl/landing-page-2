f83c09f6b0c860a6887307823e60ce22074ad219
juan01-ctrl <juanierace2@gmail.com>
2021-01-12 23:14:14 -0300

    Updated

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -46,7 +46,7 @@ document.addEventListener('click',e=>{
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/your@email.com", {
+    fetch("https://formsubmit.co/ajax/juanierace74@gmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
